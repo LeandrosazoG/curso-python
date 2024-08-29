f6b393f03609243672a962dd1b7afbdd3ea6dadc
Leandro Sazo <leandrosazo2014@gmail.com>
2024-08-29 15:03:57 -0400

    proyecto avanzado

--- dia4/menu_banco.py
+++ dia4/menu_banco.py
@@ -1,6 +1,7 @@
 ## CREANDO MENU
 print("BIENVENIDO AL BANCO LEANDRO \n")
 
+saldo=50000
 
 contraseña_correcta=123
 
@@ -19,14 +20,21 @@ while True:
         
 while True:
     opciones=int(input("ingrese una opcion "))
-    if opciones == 1:
-        print("TU ESTADO DE CUENTA SON $50000 ")
+    if opciones == 1: 
+        print(f"tu saldo contable es de : {saldo}")
+        break
     elif opciones == 2:
         print("ENTRASTE A TU CUENTA CORRIENTE CORRECTAMENTE")
+        break
     elif opciones == 3: 
+        saldo_retiro=int(input("ingresa cuando es el monto que deseas retirar :"))
+        saldo_final= saldo-saldo_retiro
+        print(f"TU ESTADO DE CUENTA SON :{saldo_final}")
         print("TU DINERO SERA RETIRADO EN BREVE")
+        break
     elif opciones == 4:
         print("tus pagos se recivieron correctamente ")
+        break
     elif opciones == 5:
         print ("tu comprobante sera imprimido en breve ")
         break
