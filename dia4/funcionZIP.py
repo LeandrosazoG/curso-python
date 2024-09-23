8391a1d9c4549a72853cf1c20897d74b29e41f17
Leandro Sazo <leandrosazo2014@gmail.com>
2024-09-23 16:20:56 -0300

    agregando nuevos ejercicios a dia 3

--- dia4/funcionZIP.py
+++ dia4/funcionZIP.py
@@ -32,3 +32,15 @@ combinado = list(zip(capitales,paises))
 
 for capitales,paises in combinado:
     print(f'La capital de {paises } es {capitales}')
+
+
+
+
+##### 
+# Listas de números en diferentes idiomas
+espanol = ['uno', 'dos', 'tres', 'cuatro', 'cinco']
+portugues = ['um', 'dois', 'três', 'quatro', 'cinco']
+ingles = ['one', 'two', 'three', 'four', 'five']
+
+# Creando el objeto zip y convirtiéndolo en una lista
+numeros = list(zip(espanol, portugues, ingles))
